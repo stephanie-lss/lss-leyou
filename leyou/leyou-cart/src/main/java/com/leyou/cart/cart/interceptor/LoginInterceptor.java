11faafd7cda04442e8f4667ee5512481d6131c13
lisheng <892958173@qq.com>
2019-11-22 17:47:50 +0800

    finished：未登录状态下的购物车 next：登录状态下的购物车

---
+++ leyou/leyou-cart/src/main/java/com/leyou/cart/cart/interceptor/LoginInterceptor.java
@@ -0,0 +1,53 @@
+package com.leyou.cart.cart.interceptor;
+
+import com.leyou.auth.pojo.UserInfo;
+import com.leyou.auth.utils.JwtUtils;
+import com.leyou.cart.config.JwtProperties;
+import com.leyou.commom.utils.CookieUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author LiSheng
+ * @date 2019/11/21 21:37
+ */
+@Component
+@EnableConfigurationProperties(JwtProperties.class)
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    //线程变量
+    private static final ThreadLocal<UserInfo> THREAD_LOCAL = new ThreadLocal<>();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取cookie中的token
+        String token = CookieUtils.getCookieValue(request, this.jwtProperties.getCookieName());
+        //解析token，获取用户信息
+        UserInfo userInfo = JwtUtils.getInfoFromToken(token, this.jwtProperties.getPublicKey());
+        if (userInfo == null) {
+            return false;
+        }
+        //把userInfo放入线程局部变量
+        THREAD_LOCAL.set(userInfo);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //清除线程的局部变量，因为使用的是tomcat的线程池，线程不会结束，也就不会释放线程的局部变量
+        THREAD_LOCAL.remove();
+    }
+
+    public static UserInfo getUserInfo(){
+        return THREAD_LOCAL.get();
+    }
+
+}
---
+++ leyou/leyou-cart/src/main/java/com/leyou/cart/config/LeyouWebMvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.leyou.cart.config;
+
+import com.leyou.cart.cart.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author LiSheng
+ * @date 2019/11/21 22:12
+ */
+@Configuration
+public class LeyouWebMvcConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**");
+    }
+}
